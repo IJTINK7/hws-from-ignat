e6d00fcf78afb3a068fe880226e964b81657f326
IJTINK7 <ijtink7@mail.ru>
2022-11-14 20:13:56 +0300

    Add "MessagePropsType" typification and import "MessageType" typification from HW1.tsx file

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import s from './Message.module.css'
+import {MessageType} from "../HW1";
 
 // нужно создать правильный тип вместо any
-export type MessagePropsType = any
 
+export type MessagePropsType = {
+    message: MessageType;
+}
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
     return (
@@ -11,6 +14,7 @@ const Message = (props: MessagePropsType) => {
             <div className={s.imageAndText}>
                 <img
                     id={'hw1-avatar-' + props.message.id}
+                    alt={"avatar"}
                     // создаёт студент
 
                     //
