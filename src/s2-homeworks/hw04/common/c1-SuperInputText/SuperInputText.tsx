00c6de1167e49a7eeeec8774a66b023d464eb714
IJTINK7 <ijtink7@mail.ru>
2022-12-16 19:51:07 +0300

    Change the style of input error

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -48,14 +48,18 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
 		onEnter() // то вызвать его
 	}
 
-	const finalSpanClassName = s.error
-		+ (spanClassName ? ' ' + spanClassName : '')
-	const finalInputClassName = s.input
-		+ (error ? ' ' + s.errorInput : ' ' + s.superInput)
+	const finalSpanClassName = s.error + (spanClassName ? ' ' + spanClassName : '')
+	const finalInputClassName = s.input + (error ? ' ' + s.errorInput : ' ' + s.superInput)
 		+ (className ? ' ' + s.className : '') // задача на смешивание классов
 
 	return (
 		<div className={s.inputWrapper}>
+			<div
+				id={id ? id + '-span' : undefined}
+				className={finalSpanClassName}
+			>
+                {error}
+            </div>
 			<input
 				id={id}
 				type={'text'}
@@ -64,12 +68,6 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
 				className={finalInputClassName}
 				{...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
 			/>
-			<span
-				id={id ? id + '-span' : undefined}
-				className={finalSpanClassName}
-			>
-                {error}
-            </span>
 		</div>
 	)
 }
