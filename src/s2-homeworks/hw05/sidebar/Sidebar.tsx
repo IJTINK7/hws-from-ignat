17fcbbe14e69d2fed78c97b7bf4fb2bebebd71ea
IJTINK7 <ijtink7@mail.ru>
2023-02-27 23:44:15 +0300

    Adding styles for active and while hover links

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -10,8 +10,7 @@ type PropsType = {
 }
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
-    const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
+    const sidebarClass = s.sidebar + (open ? ' ' + s.open : '')
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -31,7 +30,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({isActive})=>isActive ? s.active : ' ' } // делает студент
                     >
                         Pre-junior
                     </NavLink>
