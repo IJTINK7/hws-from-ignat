903aa71d585a178fb1fe7bf6321bdab5c0a187a9
IJTINK7 <ijtink7@mail.ru>
2023-03-15 15:50:17 +0300

    Changing className for links

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -30,7 +30,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={({isActive})=>isActive ? s.active : s.nav } // делает студент
+                        className={({isActive}) => isActive ? s.active : s.a} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -38,7 +38,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={({isActive})=>isActive ? s.active : s.nav } // делает студент
+                        className={({isActive}) => isActive ? s.active : s.a} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -46,7 +46,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={({isActive})=>isActive ? s.active : s.nav } // делает студент
+                        className={({isActive}) => isActive ? s.active : s.a} // делает студент
                     >
                         Junior Plus
                     </NavLink>
