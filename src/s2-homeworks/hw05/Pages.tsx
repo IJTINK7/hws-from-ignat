07fa26fb1f4452da6be555480de08be27df122e6
IJTINK7 <ijtink7@mail.ru>
2023-03-15 16:16:30 +0300

    Changing Routes

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -6,9 +6,9 @@ import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
 
 export const PATH = {
-    PRE_JUNIOR: '/pre-junior',
-    JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+    PRE_JUNIOR: '/hws-from-ignat/pre-junior',
+    JUNIOR: '/hws-from-ignat/junior',
+    JUNIOR_PLUS: '/hws-from-ignat/junior-plus',
 }
 
 function Pages() {
@@ -18,15 +18,15 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path={"/"} element={<Navigate to={'/pre-junior'}/>}/>
+                <Route path={"/hws-from-ignat/"} element={<Navigate to={'/hws-from-ignat/pre-junior'}/>}/>
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                <Route path={"/pre-junior"} element={<PreJunior/>}/>
-                <Route path={"/junior"} element={<Junior/>}/>
-                <Route path={"/junior-plus"} element={<JuniorPlus/>}/>
+                <Route path={"/hws-from-ignat/pre-junior"} element={<PreJunior/>}/>
+                <Route path={"/hws-from-ignat/junior"} element={<Junior/>}/>
+                <Route path={"/hws-from-ignat/junior-plus"} element={<JuniorPlus/>}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                <Route path={"/*"} element={<Error404/>}/>
+                <Route path={"/hws-from-ignat/*"} element={<Error404/>}/>
             </Routes>
         </div>
     )
