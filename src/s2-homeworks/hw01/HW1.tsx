8a06e7aed9b31fc34144af8b7ac08f7659fcd997
IJTINK7 <ijtink7@mail.ru>
2023-09-28 13:57:49 +0300

    Description of MessagePropsType in Message.tsx

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -7,7 +7,7 @@ import avatar from './avatar.png'
 
 /*
 * 1 - описать тип MessageType +
-* 2 - описать тип MessagePropsType в файле Message.tsx
+* 2 - описать тип MessagePropsType в файле Message.tsx +
 * 3 - в файле Message.tsx отобразить приходящие данные
 * 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
 * 5 - сделать стили в соответствии с дизайном
@@ -33,7 +33,7 @@ export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Brad',  // можно менять
     },
     message: {
         text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
