e024bb8e64b63c2aa85073f550036da6809a0540
IJTINK7 <ijtink7@mail.ru>
2023-10-04 12:38:17 +0300

    Adding types for "deleteAffair" function, covering it with tests

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -6,7 +6,7 @@ import s2 from '../../s1-main/App.module.css'
 * 1 - описать типы AffairPriorityType, AffairType +
 * 2 - указать нужный тип для defaultAffairs +
 * 3 - дописать типы и логику функции filterAffairs и проверить её тестами +
-* 4 - выполнить пункт 3 для функции deleteAffair
+* 4 - выполнить пункт 3 для функции deleteAffair +
 * 5 - указать нужный тип в useState с affairs
 * 6 - дописать тип и логику функции deleteAffairCallback
 * 7 - в файле Affairs.tsx дописать типизацию пропсов
@@ -38,9 +38,8 @@ const defaultAffairs: AffairType[] = [ // need to fix any
 export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => { // need to fix any
     return affairs.filter(el => el.priority === filter) // need to fix
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-
-    return affairs // need to fix
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { // need to fix any
+    return affairs.filter(el => el._id !== _id) // need to fix
 }
 
 function HW2() {
