ca542b6e64c21118488a723f1f5a651a2b58aca2
IJTINK7 <ijtink7@mail.ru>
2023-10-04 12:39:40 +0300

    Adding typification for "affairs" useState

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -7,7 +7,7 @@ import s2 from '../../s1-main/App.module.css'
 * 2 - указать нужный тип для defaultAffairs +
 * 3 - дописать типы и логику функции filterAffairs и проверить её тестами +
 * 4 - выполнить пункт 3 для функции deleteAffair +
-* 5 - указать нужный тип в useState с affairs
+* 5 - указать нужный тип в useState с affairs +
 * 6 - дописать тип и логику функции deleteAffairCallback
 * 7 - в файле Affairs.tsx дописать типизацию пропсов
 * 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
@@ -43,7 +43,7 @@ export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] =
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
